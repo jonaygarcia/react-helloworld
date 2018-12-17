6e6642dc6255278db7f64412f3aea487d4492c56
Jonay Garcia <jonay.garcia@gmail.com>
2018-12-17 17:45:50 +0000

    Commit 3

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-            Esto es un comentario.
+            Esto es un comentario2.
           </p>
           <a
             className="App-link"
